50eda2aca6827c1d968ddc851a776f0d05c0159f
Matias <mail@dh.com>
2019-12-11 20:55:26 -0300

    fin del taller

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
 app.get('/', (req,res) => {
-    res.send('<h1>Hola Mundo!</h1>');
+    //res.sendFile(__dirname + '/views/index.html');
+    res.render('index', {'nombre':'Matias'});
 });
 
 app.get('/about', (req,res) => {
